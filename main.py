b02a22c00b6c2a0e7d3bdded31512ae2385874b0
Reasondro <alessandrohasian16@gmail.com>
2024-11-12 01:06:06 +0700

    add main

--- main.py
+++ main.py
@@ -5,13 +5,19 @@ from fastapi import FastAPI
 app = FastAPI()
 
 
+
 @app.get("/")
-def read_root():
-    return {"Hello": "World"}
-
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+async def root():
+    return {
+        "Nama" : "Alessandro JH",
+        "NIM" : "18222025",
+        "Bisnis" : "ToFood",
+        "Recipe" : "Soto Barbeque",
+        "Recipe Screening": "Passed",
+        "Recipe Recomendation": "Add more spicess"
+            }
+# @app.get("/items/{item_id}")
+# def read_item(item_id: int, q: Union[str, None] = None):
+#     return {"item_id": item_id, "q": q}
 
     
